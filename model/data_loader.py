accce6c6a39ad8c789b1db0d599130b80fac9b42
Amy <amy.sudarat@gmail.com>
2018-12-20 20:15:10 -0500

    create test function to parse data

--- model/data_loader.py
+++ model/data_loader.py
@@ -21,8 +21,16 @@ def load_facial_graph():
         else:
             input_samples = input_samples.append(face,ignore_index=True)
     
+def load_facial_graph_test():
+    path = "C:\\Users\\DSPLab\\Research\\affective-monitor-model\\data\\test\\FacialPoints.txt"    
+    face = pd.read_csv(path,header=2,parse_dates=True,delimiter=",",quotechar=";")
+    return face
     
+        
     
     
 if __name__ == "__main__":
-    load_facial_graph()
+#    testpath = "
+#    load_facial_graph()
+    face = load_facial_graph_test()
+    
