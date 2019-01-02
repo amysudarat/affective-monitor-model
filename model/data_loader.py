6128c55cb69d16a1d2ceeea85adf1df3b5c0641f
Amy <amy.sudarat@gmail.com>
2019-01-01 21:37:21 -0500

    dataset_class work correctly

--- model/data_loader.py
+++ model/data_loader.py
@@ -4,48 +4,10 @@
 specifies how the data should be fed to the network
 """
 
-import pandas as pd
-import numpy as np
-import torch
 from dataset_class import AffectiveMonitorDataset
-import torch.utils.data.dataset as Dataset
 import matplotlib.pyplot as plt
 from mpl_toolkits.mplot3d import Axes3D
 
-
-#
-#def load_facial_graph():
-#    path = "E:\\Research\\affective-monitor-model\\data\\"
-#    filepaths = [os.path.join(path, "TestSubject"+str(i)+"\\FacialPoints.txt") for i in range(2,4)]
-#    input_samples = pd.DataFrame()
-#    for filepath in filepaths:
-#        face = pd.read_csv(filepath,header=2,parse_dates=True,delimiter=",")
-#        if input_samples.empty:
-#            input_samples = face
-#        else:
-#            input_samples = input_samples.append(face,ignore_index=True)
-#    
-#def load_facial_graph_test():
-#    path = "C:\\Users\\DSPLab\\Research\\affective-monitor-model\\data\\TestSubject2\\Data.txt"    
-#    face_df = pd.read_csv(path,header=2,delimiter=",",quotechar=";",index_col="PicIndex",skipinitialspace=True)
-#    # convert string to tuple
-##    face_df.shape[1]-1
-#    for i in range(0,1347):
-#        face_df.iloc[:,i] = pd.Series([ast.literal_eval(x) for x in face_df.iloc[:,i]]) 
-#    
-#    return face_df
-#
-#def plot_face(face):
-#    fig = plt.figure()
-#    ax = fig.add_subplot(111, projection='3d')
-#    face = list(face.iloc[0:1347])
-#    
-#    ax.scatter(*zip(*face),c='r')   
-#    ax.set_xlabel('X')
-#    ax.set_ylabel('Y')
-#    ax.set_zlabel('Z')
-#   
-#    plt.show()
 
 def plot_face(face):
         """
@@ -69,7 +31,8 @@ if __name__ == "__main__":
 #    testpath = "
 #    load_facial_graph()
     
-    face_dataset = AffectiveMonitorDataset("C:\\Users\\DSPLab\\Research\\affective-monitor-model\\data\\TestSubject2\\Data.txt")
-    data = face_dataset.face_frame
-    plot_face(face_dataset.face_frame.iloc[0])
+#    face_dataset = AffectiveMonitorDataset("C:\\Users\\DSPLab\\Research\\affective-monitor-model\\data")
+    face_dataset = AffectiveMonitorDataset("E:\\Research\\affective-monitor-model\\data")
+    data = face_dataset[0]
+#    plot_face(face_dataset.face_frame.iloc[0])
     
--- model/dataset_class.py
+++ model/dataset_class.py
@@ -3,11 +3,8 @@
 import os
 import ast
 import pandas as pd
-import numpy as np
-import torch
 from torch.utils.data import Dataset
-import matplotlib.pyplot as plt
-from mpl_toolkits.mplot3d import Axes3D
+
 
 class AffectiveMonitorDataset(Dataset):
     """ Affective Monitor Dataset """
@@ -15,25 +12,69 @@ class AffectiveMonitorDataset(Dataset):
     def __init__(self,filepath,transform=None):
         """
         Args:
-            filepath (string): Path to data input textfile
+            filepath (string): Path to data directory
             transform (callable,optional): optional transform to be applied on a sample.
         """
-        self.face_frame = self.load_dataframe(filepath)
+        # map pic index with arousal level and 
+        self.label_lookup = self.load_label(filepath)
+        # load samples from csv file
+        self.samples = self.load_dataframe(filepath)
+        # option for data augmentation
         self.transform = transform
+    
 
     def load_dataframe(self,path):
-#        path = "C:\\Users\\DSPLab\\Research\\affective-monitor-model\\data\\test\\FacialPoints.txt"    
-        face_df = pd.read_csv(path,header=2,delimiter=",",quotechar=";",index_col="PicIndex",skipinitialspace=True)
-        # convert string to tuple
-        for i in range(0,1347):
-            face_df.iloc[:,i] = pd.Series([ast.literal_eval(x) for x in face_df.iloc[:,i]])         
-        return face_df
+        """
+        Read CSV file and convert it to appropriate type
+        """
+        # create file path 
+        filepaths = [os.path.join(path, "TestSubject"+str(i)+"\\Data.txt") for i in range(2,4)]
+        
+        # initialize Total dataframe
+        total = pd.DataFrame()
+        # Loop through each Testsubject folder
+        for filepath in filepaths:
+            face_df = pd.read_csv(filepath,header=2,delimiter=",",quotechar=";",index_col="PicIndex",skipinitialspace=True)
+            # convert string to tuple
+            for i in range(0,1347):
+                face_df.iloc[:,i] = pd.Series([ast.literal_eval(x) for x in face_df.iloc[:,i]]) 
+            # create face sample loop through each picture index
+            for i in range(1,4):
+                # group sequence of face point
+                face_per_picture = face_df.loc[i]
+                face_points_per_picture = face_per_picture.iloc[:,0:1347]
+                face_points_in_sequence = []
+                for j in range(face_points_per_picture.shape[0]):
+                    face_points_in_sequence.append(list(face_points_per_picture.iloc[j]))
+                # prepare pupil diameter
+                pupils = list(face_per_picture.loc[:,"PupilDiameter"])
+                # prepare illuminance
+                illuminance = list(face_per_picture.loc[:,"Illuminance"])               
+                # create one sample
+                sample = {'facepoints': face_points_in_sequence,
+                          'PD': pupils,
+                          'illuminance': illuminance,
+                          'arousal': self.label_lookup.loc[i,'Arousal_mean(IAPS)'],
+                          'valence': self.label_lookup.loc[i,'Valence_mean(IAPS)'] }
+                # append prepared sample to total dataframe
+                total = total.append(sample, ignore_index=True)
+        return total
+    
+    def load_label(self,path):
+        filepath_label = os.path.join(path, "TestSubject2\\SAMrating.txt") 
+        SAM_df = pd.read_csv(filepath_label,header=1,index_col="PictureIndex")
+        return SAM_df
+
+    def preprocess_pupil(self):
+        pass
    
     def __len__(self):
-        return len(self.face_frame)
+        return len(self.samples)
     
     def __getitem__(self,idx):
-        
-         return
-        
+        sample = self.samples.iloc[idx]
+        if self.transform:
+            sample= self.transform(sample)
+        return sample
+
 
