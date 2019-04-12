98bc1c885a17a6e3fb72f0c0ee953f15dbf186be
Amy <amy.sudarat@gmail.com>
2019-04-11 23:40:35 -0400

    pad glitch finish

--- pd_sandbox.py
+++ pd_sandbox.py
@@ -9,5 +9,17 @@ face_dataset = utils.load_object("data_1_50_fixPD_Label_False.pkl")
 #sample_idx = 583
 sample_idx = 583
 pd_signal = face_dataset[sample_idx]['PD_avg_filtered']
-processed_pd = ppd.smooth_differentiator(pd_signal)
-ppd.plot_pd_before_after(face_dataset[sample_idx],processed_pd)
+
+## np diff
+#processed_pd = ppd.differentiator(pd_signal)
+## np gradient
+#processed_pd = ppd.gradient(pd_signal)
+#ppd.plot_pd_before_after(face_dataset[sample_idx],processed_pd)
+
+## smooth curve
+#processed_pd = ppd.savgol(pd_signal,11,4)
+#ppd.plot_pd_before_after(face_dataset[sample_idx],processed_pd,adjust=False)
+
+# detect glitch
+glitch_index, processed_pd = ppd.detect_glitch(pd_signal)
+ppd.plot_pd_before_after(face_dataset[sample_idx],processed_pd,adjust=False,glitch_index=glitch_index)
--- preprocessing/pd.py
+++ preprocessing/pd.py
@@ -1,20 +1,59 @@
 # -*- coding: utf-8 -*-
 
 import numpy as np
-import pandas as pd
+#import pandas as pd
 import matplotlib.pyplot as plt
+import scipy.signal
+
+
+def detect_glitch(raw, threshold=1.5):
+    
+    # pass it to differentiator
+    diff_raw = differentiator(raw)
+    
+    # detect where is the glitch
+    glitch_index = [i for i in range(len(diff_raw)) if diff_raw[i] > threshold or diff_raw[i] < -1*threshold]
+    
+    # check if between indexes if the diff value is zero or not (no change in between)
+    # then replace it with average
+    i = 0
+    output = raw
+    while i+1 < len(glitch_index): # 2<3
+        start = glitch_index[i]+1 
+        stop = glitch_index[i+1]
+        if sum(diff_raw[start:stop]) == 0:
+            replace_value = (raw[start-1]+raw[stop+1])/2
+            replace_list = [replace_value for i in range(stop-start+1)]
+            output = raw[:start]+replace_list+raw[stop+1:]
+            assert len(raw) == len(output), "fix slicing list"
+        i = i+1 # 2
+    
+
+    return glitch_index, output
 
 
 
 
-def smooth_differentiator(pd_signal):
+def differentiator(pd_signal):
     
     output = np.diff(pd_signal)
       
     return output
 
+def gradient(pd_signal):
+    
+    output = np.gradient(pd_signal)
+      
+    return output
 
-def plot_pd_before_after(sample,processed_pd=None,ax=None):
+def savgol(pd_signal,window=15,polyorder=2):
+    
+    output = scipy.signal.savgol_filter(pd_signal,window,polyorder)
+      
+    return output
+
+
+def plot_pd_before_after(sample,processed_pd=None,ax=None,adjust=True,glitch_index=None,):
     
     if ax is None:        
         ax = plt.axes()
@@ -31,8 +70,13 @@ def plot_pd_before_after(sample,processed_pd=None,ax=None):
     ax.text(0, pd_merge[0], str(arousal), bbox=dict(facecolor='red', alpha=0.5))
     ax.plot(pd_merge,'k')
     if processed_pd is not None:
-        processed_pd = [i+avg for i in processed_pd]
-        ax.plot(processed_pd,'r',linewidth=2)
+        if adjust:
+            processed_pd = [i+avg for i in processed_pd]
+        ax.plot(processed_pd,'--r',linewidth=2)
+    if glitch_index is not None:
+        x = glitch_index
+        y = [pd_merge[i] for i in glitch_index]
+        ax.plot(x,y,'bo')
 #    ax.plot(pd_left,'--r')
 #    ax.plot(pd_right,'--b')
 #    ax.plot(zero_line,'y')
